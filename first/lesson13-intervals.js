e36c579673a07b2d0dc581f4ca3e8b14ffc8e360
DianaUzun <dianauzun2006@gmail.com>
2023-06-20 20:42:00 +0300

    learn intervals and timeout

---
+++ first/lesson13-intervals.js
@@ -0,0 +1,25 @@
+
+// INTERVAL
+var counter = 0;
+
+// function func() {
+//     counter++;
+//     console.log(counter);
+//     if (counter > 10) {
+//         clearInterval(go);
+//     }
+// }
+// let go = setInterval(func, 1000);
+
+// setInterval( function() {
+//     counter++;
+//     console.log(counter);
+// }, 2000);
+
+
+
+//TIMEOUT
+
+setTimeout(function(){
+    console.log("Hiii");
+},3000);
